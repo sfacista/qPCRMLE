ab80987a6a65115832d21ef198eeae24f53ea44f
sfacista <sfacista@tgen.org>
2018-09-17 10:39:58 -0700

    working script
    this is the new working version of the script. plotting would provide clarity as to what is actually being calculated have not verified the validity of the calculations. optputs an answer file, but last line could be commented out for all practical purposes

--- MLE.m
+++ MLE.m
@@ -1,21 +1,40 @@
 %here we read in the file of interest, it has been placed in the working
 %director labeled "MATLAB".
 
-datamatrix = dlmread('data.txt');
+userinput2 = "'Format your input data into a 3-column, tab or space delimited, plain-text file with columns representing sample ID, cycle number, and delta Rn, respectively.\nEnsure that the number of cycles is the same for all samples or this script will not work properly.\nDo not include labels in your data file.\nSpecify absolute path with filename or relative filename (for files in this script's directory) now please:  \n";
+datafile = input(userinput2,'s')
+datamatrix = dlmread(datafile);
+userinput1 = 'On what cycle number does the fluorescent signal exceed the background level (What is your Ct value)?\nHint: It is important that this Ct value be uniform for all samples. \n ' ;
+beginoncycle = input(userinput1);
+%filtering datamatrix to only include ct of interest
+datamatrix2 = datamatrix(datamatrix(:,2) >= beginoncycle(1,1),:);
+totaluniquesamples = size(unique(datamatrix2(:,1)),1);
+totalrows = size(datamatrix2,1);
+numbercyclespersample = totalrows/totaluniquesamples;
+divisionmultiplier=[1:totaluniquesamples];
+divisionindexes=divisionmultiplier.*numbercyclespersample;
 
-%various QC metrics
+%various QC metrics no needed unless debugging
 %fooa = datamatrix(:,1);
 %foo  = unique(fooa);
-%fo = transpose(foo);
-
-%plot of data (assuming that the data to be plotted is in the subsequent
-%columns)
-x=datamatrix(:,2);
-y=datamatrix(:,3);
-plotmatrix(x,y)
 
 %histogram(datamatrix(:,end))
 
-%MLE of the last column of data. This assumes that the data is in a
-%standard qPCR configuration (logistic function)
-mle(datamatrix(:,end), 'distribution', 'logistic')
+i = 1;
+while i < (size(datamatrix2,1)-numbercyclespersample+1)
+    answ(i,:) = mle(datamatrix2(i:i+numbercyclespersample-1,3),'distribution', 'logistic');
+    i = i+numbercyclespersample-1;
+end
+
+%k=1;
+%while k < (size(datamatrix2,1)-numbercyclespersample+1);
+%    plotmatrix((datamatrix2(k:k+numbercyclespersample-1,2)),(datamatrix2(k:k+numbercyclespersample-1,3)));
+%    hold on;
+%    k = k+numbercyclespersample-1;
+%end --- this part is unfinished
+
+answerlist = answ(1:numbercyclespersample-1:end,1:2);
+samplelist = unique(datamatrix2(:,1));
+disp = 'Here are your answers. The first column is the label you started with (usually a well number). The second column is the scale parameter (alpha). The third column is the shape parameter for the logistic distribution.'
+finalanswer = [samplelist answerlist]
+dlmwrite('mle_output.txt', finalanswer)
